923b7799f7c013531d2da98fa61b6527575840cb
Brook <yuefengkai@163.com>
2019-04-11 09:54:33 +0800

    Initialization

---
+++ ResourceOwnerPassword/Program.cs
@@ -0,0 +1,88 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ResourceOwnerPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                await Start();
+            });
+
+            Console.ReadKey();
+        }
+
+        static async Task Start()
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+
+                    if (disco.IsError)
+                    {
+                        Console.WriteLine(disco.Error);
+                        return;
+                    }
+
+                    var tokenResource = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+                    {
+                        Address = disco.TokenEndpoint,
+
+                        ClientId = "ro.client",
+                        ClientSecret = "secret",
+
+                        UserName="aa",
+                        Password= "123",
+                        Scope = "myApi",
+                    });
+
+
+                    if (tokenResource.IsError)
+                    {
+                        Console.WriteLine(tokenResource.Error);
+                        return;
+                    }
+
+                    Console.WriteLine(tokenResource.Json);
+
+                    await Send(tokenResource.AccessToken);
+
+                    async Task Send(string token)
+                    {
+                        using (var client2 = new HttpClient())
+                        {
+                            client2.SetBearerToken(token);
+
+                            var response = await client2.GetAsync($"http://localhost:5001/api/values/{DateTime.Now.ToString("yyyyMMddHHmmss")}");
+
+                            if (!response.IsSuccessStatusCode)
+                            {
+                                Console.WriteLine(response.StatusCode);
+                                return;
+                            }
+                            else
+                            {
+                                var content = await response.Content.ReadAsStringAsync();
+
+                                Console.WriteLine($"result:{content}");
+                            }
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"There was an exception: {ex.ToString()}");
+            }
+
+        }
+    }
+}
---
+++ identityTest/Config.cs
@@ -0,0 +1,58 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickstartIdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[] {
+                new IdentityResources.OpenId()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new ApiResource[] {
+                new ApiResource("myApi","first api")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client> {
+                new Client{
+                    ClientId="clientid",
+                    AllowedGrantTypes= GrantTypes.ClientCredentials,
+                    ClientSecrets=
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes={ "myApi" }
+                } ,
+                new Client{
+                    ClientId="ro.client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    ClientSecrets=
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes={ "myApi" }
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser> {
+                new TestUser{ SubjectId="1",Username="aa",Password="123"},
+                new TestUser{ SubjectId="2",Username="bb",Password="123"}
+            };
+        }
+    }
+}
